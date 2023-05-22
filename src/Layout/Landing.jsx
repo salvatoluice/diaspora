0a01d73a780be36a0caf616fe0f73c323513da35
salvatoluice <salvatoluice5@gmail.com>
2023-05-22 22:58:19 +0300

    Hero section

--- src/Layout/Landing.jsx
+++ src/Layout/Landing.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Hero from '../Components/Hero'
 
 const Landing = () => {
   return (
     <div className='background'>
       <Navbar />
+      <Hero />
     </div>
   )
 }
